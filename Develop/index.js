010e8722cfe592ac536981ddcc5947a4ee7c6991
Kevin Choi <kevchoi1028@gmail.com>
2021-07-16 13:44:51 -0700

    sample markdown was created

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,91 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Create an array of questions for user input
+const questions = [
+    {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "projectTitle"
+    },
+    {
+        type: "input",
+        message: "Please provide brief description of your project.",
+        name: "projectDescription"
+    },
+    {
+        type: "input",
+        message: "Please provide an installation instruction.",
+        name: "installationInstruction"
+    },
+    {
+        type: "input",
+        message: "Please provide an usage information",
+        name: "usageInstruction"
+    },
+    {
+        type: "list",
+        message: "Please choose a license from the list below:",
+        choices: ["MIT License", "Apache License 2.0", "Mozilla Public License 2.0", "GNU General Public License, version 3", "No License"],
+        name: "license"
+    },
+    {
+        type: "input",
+        message: "Please enter current year.",
+        name: "year"
+    },
+    {
+        type: "input",
+        message: "Please enter your full name.",
+        name: "fullname"
+    },
+    {
+        type: "input",
+        message: "Please provide a contribution guideline.",
+        name: "contribution"
+    },
+    {
+        type: "input",
+        message: "Please provide a test instruction.",
+        name: "test"
+    },
+    {
+        type: "input",
+        message: "Please enter your email.",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "Please enter your github username.",
+        name: "github"
+    },
+    {
+        type: "input",
+        message: "Please enter your linkedin username.",
+        name: "linkedin"
+    }
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Users are prompted with questions and get their answers
+const userPrompt = () => {
+    inquirer.prompt((questions)).then((answers) => {
+        writeToFile('readme_generator.md', generateMarkdown(answers));
+    });
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        err ? console.log(err):console.log("It successfully created README file!")
+    });
+};
+
+// Create a function to initialize app
+function init() {
+    userPrompt();
+};
 
 // Function call to initialize app
 init();
---
+++ Develop/readme_generator.md
@@ -0,0 +1,52 @@
+# s
+
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+ 
+  ## Installation
+  s
+
+  ## Usage
+  s
+
+  
+  ## License
+  This project is covered under MIT License.
+
+  <details>
+    <summary>
+      See License
+    </summary> 
+  
+  ```
+  Copyright <s> <s>
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+  
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+  ```
+  </details>
+  
+
+  ## Contributing
+  s
+
+  ## Tests
+  s
+
+  ## Questions
+  If you have any questions, please contact me via
+
+  * Email: [s](mailto:s)
+
+  * GitHub: [https://github.com/s](https://github.com/s)
+
+  * Linkedin: [https://linkedin.com/in/s](https://linkedin.com/in/s)
